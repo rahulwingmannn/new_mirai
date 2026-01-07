b1cda06d9c3b9bb5b7bc70556849b166c1689d42
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-07 15:25:23 +0530

    Add files via upload

---
+++ app/the-luxury-of-breathing/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TheLuxuryOfBreathing from '@/components/the-luxury-of-breathing/page'
+
+const page = () => {
+  return (
+    <TheLuxuryOfBreathing />
+  )
+}
+
+export default page
