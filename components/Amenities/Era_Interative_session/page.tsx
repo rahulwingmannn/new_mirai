b8c48e19680f9b5674fce58dad64cf852b8cf733
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-09 13:30:16 +0530

    Update page.tsx

--- components/Amenities/Era_Interative_session/page.tsx
+++ components/Amenities/Era_Interative_session/page.tsx
@@ -124,10 +124,10 @@ const EraSeasons: React.FC = () => {
   ]);
 
   const backgroundImages: { [key: string]: string } = {
-    'bg-spring': 'https://d3p1hokpi6aqc3.cloudfront.net/Avia_Pod_1.png',
-    'bg-summer': 'https://d3p1hokpi6aqc3.cloudfront.net/AQUA%20POD_1.png',
+    'bg-spring': 'https://d3p1hokpi6aqc3.cloudfront.net/AQUA%20POD_1.png',
+    'bg-summer': 'https://d3p1hokpi6aqc3.cloudfront.net/Avia_Pod_1.png',
     'bg-autumn': 'https://d3p1hokpi6aqc3.cloudfront.net/Aminites_Tera_Pods_1.png',
-    'bg-winter': 'https://d3p1hokpi6aqc3.cloudfront.net/Pyro_Pod_1.png',
+    'bg-winter': 'https://azure-baboon-302476.hostingersite.com/mirai_latest/media/pods/pyro_pod.png',
     'bg-terra-pool': 'https://pavanimirai.com/55storeys-luxury-apartments-in-financial-district/media/infinitypool.jpg',
     'bg-terra-vantage': 'https://pavanimirai.com/55storeys-luxury-apartments-in-financial-district/media/vantagepoint.jpg',
     'bg-terra-jacuzzi': 'https://pavanimirai.com/55storeys-luxury-apartments-in-financial-district/media/jacuzzi.jpg',
@@ -137,7 +137,7 @@ const EraSeasons: React.FC = () => {
     // Terra images
     'bg-terra-1': 'https://d3p1hokpi6aqc3.cloudfront.net/Aminites_Tera_Pods_1.png',
     'bg-terra-2': 'https://d3p1hokpi6aqc3.cloudfront.net/Aminites_Tera_Pods_2.png',
-    'bg-terra-3': 'https://d3p1hokpi6aqc3.cloudfront.net/Aminites_Tera_Pods_3.png',
+    'bg-terra-3': 'https://azure-baboon-302476.hostingersite.com/mirai_latest/media/pods/terravantage.png',
     // Aqua images
     'bg-aqua-1': 'https://d3p1hokpi6aqc3.cloudfront.net/AQUA%20POD_1.png',
     'bg-aqua-2': 'https://d3p1hokpi6aqc3.cloudfront.net/AQUA%20POD_2.png',
