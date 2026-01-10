437c266b17bc831ebedb56200acf9a7dda6bbe9f
rahulwingmannn <rahul@wingmanbrandworks.com>
2026-01-10 14:21:42 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           }}
         />
         
-        {/* CRITICAL: Preload hero assets for faster loading */}
+        {/* CRITICAL: Preload hero assets for faster first load */}
         <link 
           rel="preload" 
           href="/images/logo_1.png" 
@@ -42,15 +42,37 @@ export default function RootLayout({
         />
         <link 
           rel="preload" 
+          href="/images/sixth_ment.png" 
+          as="image"
+        />
+        <link 
+          rel="preload" 
+          href="/images/gateway/reveal.png" 
+          as="image"
+        />
+        <link 
+          rel="preload" 
+          href="/images/gateway/mirai.png" 
+          as="image"
+        />
+        <link 
+          rel="preload" 
+          href="/images/gateway/shape-two.png" 
+          as="image"
+        />
+        
+        {/* Preload video */}
+        <link 
+          rel="preload" 
           href="https://d3p1hokpi6aqc3.cloudfront.net/mirai_home_1.mp4" 
           as="video"
           type="video/mp4"
         />
-        <link 
-          rel="preload" 
-          href="/images/sixth_ment.png" 
-          as="image"
-        />
+        
+        {/* Preconnect to external domains */}
+        <link rel="preconnect" href="https://d3p1hokpi6aqc3.cloudfront.net" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         
         {/* CRITICAL: Reset scroll position before anything else loads */}
         <script
@@ -70,29 +92,14 @@ export default function RootLayout({
         
         {/* Playfair Display from Google */}
         <link 
-          rel="preconnect" 
-          href="https://fonts.googleapis.com" 
-        />
-        <link 
-          rel="preconnect" 
-          href="https://fonts.gstatic.com" 
-          crossOrigin="anonymous"
-        />
-        <link 
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" 
           rel="stylesheet" 
-        />
-        
-        {/* Preconnect to video CDN */}
-        <link 
-          rel="preconnect" 
-          href="https://d3p1hokpi6aqc3.cloudfront.net" 
         />
         
         {/* Favicons */}
         <link rel="icon" href="/favicon.png" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="32x32" href="/favicon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
       </head>
       <body className="antialiased" style={{ backgroundColor: '#000' }}>
         {/* Server-rendered black overlay to show immediately during initial load */}
