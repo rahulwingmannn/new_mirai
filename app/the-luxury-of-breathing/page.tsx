4f5a2a336bfe01f70b721647bf06b20a0eb694c9
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-21 12:34:34 +0530

    Integrate Footer into The Luxury of Breathing page
    Added Footer component to the page layout.

--- app/the-luxury-of-breathing/page.tsx
+++ app/the-luxury-of-breathing/page.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Footer from '@/components/Life@Mirai/Footer/page'
 import TheLuxuryOfBreathing from '@/components/the-luxury-of-breathing/page'
 
 const page = () => {
   return (
-    <TheLuxuryOfBreathing />
+    <>
+      <main className="relative z-10 bg-white mb-[100vh]">
+        <TheLuxuryOfBreathing />
+      </main>
+      
+      <Footer />
+    </>
   )
+}
 }
 
 export default page
+
