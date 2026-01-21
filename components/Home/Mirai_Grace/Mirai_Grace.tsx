8d361824568d2b3e43a3d8ffe09dd7e78a9e8f69
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-21 17:50:53 +0530

    Update Mirai_Grace.tsx

--- components/Home/Mirai_Grace/Mirai_Grace.tsx
+++ components/Home/Mirai_Grace/Mirai_Grace.tsx
@@ -5,7 +5,7 @@ const Mirai_Grace = () => {
     <div className="bg-white text-black w-full min-h-screen overflow-hidden relative flex items-center justify-center -mt-8">
       <video
         src="/highlight.mp4"
-        className="w-[105%] h-auto max-h-[105vh] object-contain scale-105"
+        className="w-[103%] h-auto max-h-[103vh] object-contain scale-103"
         autoPlay
         loop
         muted
