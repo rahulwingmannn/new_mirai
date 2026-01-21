5fe6094eb7a9054c984602b93a5bd854ff3a2e1c
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-21 12:30:52 +0530

    Refactor page component to include Footer
    Wrap AGlobalNexus component with main and add Footer.

--- app/a-global-nexus/page.tsx
+++ app/a-global-nexus/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Footer from '@/components/Life@Mirai/Footer/page'
 import AGlobalNexus from '@/components/a-global-nexus/page'
 const page = () => {
   return (
-    <AGlobalNexus />
+    <>
+      <main className="relative z-10 bg-white mb-[100vh]">
+        <AGlobalNexus />
+      </main>
+      
+      <Footer />
+    </>
   )
 }
 
 export default page
+
