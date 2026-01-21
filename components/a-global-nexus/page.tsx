27a5049c2bca6f790910277554d69cdada36b079
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-21 17:09:03 +0530

    Update page.tsx

--- components/a-global-nexus/page.tsx
+++ components/a-global-nexus/page.tsx
@@ -135,7 +135,7 @@ export default function MiraiHomesPage() {
 
           <image
             className="sky"
-            xlinkHref="https://azure-baboon-302476.hostingersite.com//mirai_/media/footer_img.png"
+            xlinkHref="https://azure-baboon-302476.hostingersite.com//mirai_/media/blog-four.jpg"
             width="1200"
             height="800"
             preserveAspectRatio="xMidYMid slice"
@@ -353,3 +353,4 @@ export default function MiraiHomesPage() {
     </main>
   );
 }
+
