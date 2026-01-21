620c2634dd7a33abb18e9fb532e6f5d813d26a0a
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-21 14:57:02 +0530

    Change background size to cover in page.tsx

--- components/Amenities/Era_Interative_session/page.tsx
+++ components/Amenities/Era_Interative_session/page.tsx
@@ -200,7 +200,7 @@ const EraSeasons: React.FC = () => {
           }`}
           style={{ 
             backgroundImage: `url(${currentBgImage})`,
-            backgroundSize: '100% 100%',
+            backgroundSize: 'cover',
             backgroundPosition: 'center center',
             backgroundRepeat: 'no-repeat'
           }}
